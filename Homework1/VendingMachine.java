4db218871ebe4922ee17038766d5af42fee6baf6
Ksenia <knopik-ksu@mail.ru>
2023-06-05 14:13:30 +0500

    homework added

---
+++ Homework1/VendingMachine.java
@@ -0,0 +1,11 @@
+
+package JAVAOOP.Homework1;
+
+interface VendingMachine {
+
+    void addDrink(hotDrink drink);
+
+    public drink getProduct(String type, int volume, int temperature);
+
+
+}
--- Lesson1/EX002/Point2D.java
+++ Lesson1/EX002/Point2D.java
@@ -1,20 +1,45 @@
 package EX002;
 
+
+/**
+ * это точка 2D
+ */
 public class Point2D {
-    int x, y;
+    private int x, y;
     public Point2D(int valueX, int valueY) {
         x = valueX;
         y = valueY;
         
     }
 
+    /**
+     *это конструктор
+     * @param valueX это координата X
+     * @param valueY это координата Y 
+     */
     public Point2D() {
-        super();
+        this(10, 11);
     }
     
 
     public Point2D(int value) {
         this(value, value);
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int value){
+        this.x=value;
+    }
+
+    public void setY(int value){
+        this.y=value;
     }
 
 
@@ -26,6 +51,10 @@ public class Point2D {
     @Override
     public String toString() {
         return getInfo();
+    }
+
+    public static double distance(Point2D a, Point2D b){
+        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
     }
     
 }
